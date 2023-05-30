b6c0816a40783d5cc5b2ee726ab333e20179d8c5
Madhavi Joshi <109155050+Madhavi210@users.noreply.github.com>
2023-05-30 19:09:07 +0530

    Update AddServlet.java

--- TODO-APP/src/main/java/com/servlet/AddServlet.java
+++ TODO-APP/src/main/java/com/servlet/AddServlet.java
@@ -4,11 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 import com.DAO.ToDoDAO;
 import com.DB.DBConnect;
-
-import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
